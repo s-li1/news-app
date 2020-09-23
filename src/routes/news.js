9034afa0f228653f63ccfa7719ad09ab847763e2
Glitch (hello-express) <none>
2020-09-23 16:23:51 +0000

    👛🏣 Checkpoint ./src/routes/news.js:22077298/104

--- src/routes/news.js
+++ src/routes/news.js
@@ -4,12 +4,12 @@ const axios = require('axios');
 
 const apiKey = 'a66afa0ac1cc42d484989eca31f4dbff';
 
-let searchQuery = "bitcoin";
+let searchQuery = "-coronvavirus-COVID19-pandemic";
 
 newsRouter.get('', async(req, res)=> {
     
     try {
-        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${apiKey}`);
+        const newsAPI = await axios.get(`https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=${apiKey}&pageSize=1`);
         const object = newsAPI.data;
         const articles = object["articles"];
         res.render('news', { articles: articles });
