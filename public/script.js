e1f52f22a078390cd073b5357ddb95b132b33391
Glitch (hello-express) <none>
2020-09-23 14:49:01 +0000

    🐁👖 Checkpoint ./src/views/news.ejs:480799/1585:22077298/1559 ./public/css/style.css:480799/29:22077298/1560 ./Procfile:22077299/35 ./src/views/style.css:480799/12 ./public/script.js:480799/4069:22077298/398

--- public/script.js
+++ public/script.js
@@ -6,11 +6,13 @@ let newsArray = [];
 
 var topicArray = [];
 
-//const apiKEY = 'a66afa0ac1cc42d484989eca31f4dbff';
+let numberr = 0
 
-//const apiURL = `https://newsapi.org/v2/top-headlines?country=us&${apiKEY}`;
+const apiKEY = 'a66afa0ac1cc42d484989eca31f4dbff';
 
-/* async function grabNews() {
+const apiURL = `https://newsapi.org/v2/top-headlines?country=us&${apiKEY}`;
+
+async function grabNews() {
   try {
     const response = await fetch(apiURL);
     
@@ -23,67 +25,20 @@ var topicArray = [];
 }
 
 grabNews();
-*/
 function progress(){
   let articletitle = "json.whatever"
   let articletext = "also that ^"
   for (var i = 1; i <= 18; i++){
   document.getElementById(i).style.display = "none"
   }
+  numberr++
+  if (numberr < 5 ){
   document.getElementById("dontshowtext").innerHTML = articletext
   document.getElementById("title").innerHTML = articletitle
-}
-
-
-function addToList(id){
-//two dimensional array, first entry is id, holds name of topic in second
-  let topicName = document.getElementById(id).innerHTML
-  if (typeof(topicArray[id]) == "string"){
-    topicArray[id] = false
-    for (var i = 0; i <= topicArray.length; i++){
-      if (topicArray[i] == topicName){
-      topicArray.splice(i)   
-      }
-    document.getElementById(id).style.color = "white"
-    }
   } else {
-    topicArray[id] = topicName
-    document.getElementById(id).style.color = "blue"
-    topicArray.push(topicName)
+  document.getElementById("dontshowtext").innerHTML = "STOP READING NOW"
+  document.getElementById("title").innerHTML = "RELAX"
   }
-  console.log(`${id} ${topicArray}`)
-}
-
-let newsArray = [];
-
-var topicArray = [];
-
-//const apiKEY = 'a66afa0ac1cc42d484989eca31f4dbff';
-
-//const apiURL = `https://newsapi.org/v2/top-headlines?country=us&${apiKEY}`;
-
-/*async function grabNews() {
-  try {
-    const response = await fetch(apiURL);
-    
-    newsArray = await response.json();
-    
-    console.log(newsArray);      
-  } catch (error) {
-    //catch error
-  }
-}
-
-grabNews();
-*/
-function progress(){
-  let articletitle = "json.whatever"
-  let articletext = "also that ^"
-  for (var i = 1; i <= 18; i++){
-  document.getElementById(i).style.display = "none"
-  }
-  document.getElementById("dontshowtext").innerHTML = articletext
-  document.getElementById("title").innerHTML = articletitle
 }
 
 
@@ -115,7 +70,3 @@ function addToList(id){
     continue
   }
   */
-
-
-
-
